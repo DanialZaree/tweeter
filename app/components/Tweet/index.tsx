3b58c9ca699e747abdb94dc44e4c99da28ccbc16
Danial Zaree <danialzaree0@gmail.com>
2026-07-30 23:02:38 +0330

    Fix build errors in Tweet component

--- app/components/Tweet/index.tsx
+++ app/components/Tweet/index.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import Link from 'next/link';
 import { useMemo, useState } from 'react';
 import { toggleTweetLike } from '@/app/lib/actions/actionLike';
 import { editTweet } from '@/app/lib/actions/tweet';
