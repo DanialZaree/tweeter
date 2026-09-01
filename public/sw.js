be03efdaa452b05a8736ec9eaa72c65f8b44c770
Danial Zaree <danialzaree0@gmail.com>
2026-09-01 23:05:32 +0330

    .gitignore

---
+++ public/sw.js
@@ -0,0 +1,39 @@
+self.addEventListener('push', (event) => {
+  const data = event.data?.json() ?? {
+    title: 'New Notification',
+    body: 'You have a new notification.',
+    url: '/'
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/icon-192x192.png',
+      badge: '/logo.svg',
+      vibrate: [200, 100, 200],
+      data: data.url,
+    })
+  );
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  const urlToOpen = event.notification.data || '/';
+
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window', includeUncontrolled: true }).then((windowClients) => {
+      for (let i = 0; i < windowClients.length; i++) {
+        const client = windowClients[i];
+
+        if (client.url.includes(self.registration.scope) && 'focus' in client) {
+          client.navigate(urlToOpen);
+          return client.focus();
+        }
+      }
+      if (self.clients.openWindow) {
+        return self.clients.openWindow(urlToOpen);
+      }
+    })
+  );
+});
